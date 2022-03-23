dc16161400c38ca4a0111ccecff64672104af1ad
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-23 11:28:40 -0700

    removed excess boilerplate items

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,9 +7,9 @@ let dex= "https://zeus.vwu.edu/~tjpepper/Pokedex/Images/PokedexClosed.png"
 function Home() {
     return (
       <div>
+         <Link to='/Pokedex/:name'><img className="homeDex" src={dex} alt="closed pokdex"/></Link>
         <h1>Welcome to the PokeDex!</h1>
         <h3>Click to open!</h3>
-        <Link to='/Pokedex/:name'><img className="homeDex" src={dex} alt="closed pokdex"/></Link>
       </div>
     );
 }
